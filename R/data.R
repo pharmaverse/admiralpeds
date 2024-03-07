90a100d6e0b6528e856cf18203ad320efec37e4b
Pierre Wallet <pierrewallet@Air-de-Pierre-001.lan>
2024-03-07 13:58:56 +0100

    #17 creation of data-raw folder to handle metadata creation #17 creation of metadata-creation.R to handle the building of metadatasets, their storage in data folder and their documentation skeleton in R/data.R. #17 creation of 10 metadatasets to reflect WHO website, with their documentation manually updated. #17 creation of snapshot tests for each metadatsets created

---
+++ R/data.R
@@ -0,0 +1,145 @@
+#' @title WHO length/height-for-age for girls
+#' @description WHO Length/height-for-age charts for girls from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{integer Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/length-height-for-age}
+"WHO_length_height_for_age_girls"
+
+#' @title WHO length/height-for-age for boys
+#' @description WHO Length/height-for-age charts for boys from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{integer Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/length-height-for-age}
+"WHO_length_height_for_age_boys"
+
+#' @title WHO weight-for-age for girls
+#' @description WHO weight-for-age charts for girls from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-age}
+"WHO_weight_for_age_girls"
+
+#' @title WHO weight-for-age for boys
+#' @description WHO weight-for-age charts for boys from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-age}
+"WHO_weight_for_age_boys"
+
+#' @title WHO weight-for-length for girls
+#' @description WHO weight-for-length charts for girls from 45cm to 110cm
+#' @format A data frame with 651 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Length}}{double Length in cm}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-length-height}
+"WHO_weight_for_length_girls"
+
+#' @title WHO weight-for-length for boys
+#' @description WHO weight-for-length charts for boys from 45cm to 110cm
+#' @format A data frame with 651 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Length}}{double Length in cm}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-length-height}
+"WHO_weight_for_length_boys"
+
+#' @title WHO weight-for-height for girls
+#' @description WHO weight-for-height charts for girls from 65cm to 120cm
+#' @format A data frame with 551 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Height}}{double Height in cm}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-length-height}
+"WHO_weight_for_height_girls"
+
+#' @title WHO weight-for-height for boys
+#' @description WHO weight-for-height charts for boys from 65cm to 120cm
+#' @format A data frame with 551 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Height}}{double Height in cm}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/tools/child-growth-standards/standards/weight-for-length-height}
+"WHO_weight_for_height_boys"
+
+#' @title WHO bmi-for-age for girls
+#' @description WHO bmi-for-age charts for girls from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/toolkits/child-growth-standards/standards/body-mass-index-for-age-bmi-for-age}
+"WHO_bmi_for_age_girls"
+
+#' @title WHO bmi-for-age for boys
+#' @description WHO bmi-for-age charts for boys from day 0 (birth) to
+#' day 1856
+#' @format A data frame with 1857 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Day}}{integer Age in days}
+#'   \item{\code{L}}{double Box-Cox transformation for normality}
+#'   \item{\code{M}}{double Median}
+#'   \item{\code{S}}{double Coefficient of variation}
+#'}
+#' @family datasets
+#' @keywords datasets
+#' @source \url{https://www.who.int/toolkits/child-growth-standards/standards/body-mass-index-for-age-bmi-for-age}
+"WHO_bmi_for_age_boys"
