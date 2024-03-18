f0c42e357bda036106f92b147fb544a898275372
Samia Kabi <57490460+kabis-ops@users.noreply.github.com>
2024-03-18 17:29:43 +0100

    Closes #19 - Testdata. Draft v0.1 (#23)
    * #19 - Testdata. Draft v0.1
    
    * #19 - Testdata. Draft v0.2
    
    ---------
    
    Co-authored-by: rossfarrugia <rossfarrugia@users.noreply.github.com>
    Co-authored-by: Zelos Zhu <zelos.zhu@atorusresearch.com>

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Demographic Dataset-updated
+#'
+#' An updated SDTM DM dataset with pediatric patients
+#'
+"dm_peds"
+
+
+#' Vital signs Dataset-updated
+#'
+#' An updated SDTM VS dataset with anthropometric measurements for pediatric patients
+#'
+"vs_peds"
