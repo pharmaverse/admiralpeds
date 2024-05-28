2ddcd8fcb7b2f160a41bc55dcdc11f08c8aa2ea8
Ross Farrugia <82581364+rossfarrugia@users.noreply.github.com>
2024-05-28 15:08:40 +0100

    minor update from Ross to apply a better label here

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -2,6 +2,6 @@ list(
   rd_family_title = list(
     metadata = "Metadata",
     datasets = "Datasets",
-    der_prm_bds_vs = "der_prm_bds_vs"
+    der_prm_bds_vs = "Vital Signs Functions for adding Parameters/Records"
   )
 )
