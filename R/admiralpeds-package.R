fbd4503f208b2e3e1d2638c3b39222ce59a3a422
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-06-17 19:03:47 +0000

    fix namespaces and setup

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -1,8 +1,8 @@
 #' @keywords internal
 #' @family internal
 #' @importFrom admiraldev assert_data_frame assert_expr assert_symbol assert_varval_list expr_c
-#' @importFrom dplyr arrange filter group_by lead left_join mutate rename select
+#' @importFrom dplyr arrange bind_rows filter group_by lead left_join mutate pull rename select
 #' @importFrom magrittr %>%
-#' @importFrom rlang enexpr exprs
+#' @importFrom rlang abort enexpr exprs is_empty
 #' @importFrom stats pnorm qnorm
 "_PACKAGE"
