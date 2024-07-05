ad25e4a589890ac0d78b8dd82df33fee7d9a8499
santarabantoosoo <31887357+santarabantoosoo@users.noreply.github.com>
2024-07-05 10:40:34 +0300

    closes #58 added warn.conflict to a all files (#61)
    * #58 added suppressPackageStartupMessages() to a single file
    
    * #58 added suppressPackageStartupMessages() to a single file
    
    * #58 added warn.conflicts to both files in references
    
    * 58 updated contribution details in description

--- R/derive_params_growth_height.R
+++ R/derive_params_growth_height.R
@@ -96,10 +96,10 @@
 #' @export
 #'
 #' @examples
-#' library(dplyr)
-#' library(lubridate)
-#' library(rlang)
-#' library(admiral)
+#' library(dplyr, warn.conflicts = FALSE)
+#' library(lubridate, warn.conflicts = FALSE)
+#' library(rlang, warn.conflicts = FALSE)
+#' library(admiral, warn.conflicts = FALSE)
 #'
 #' advs <- dm_peds %>%
 #'   select(USUBJID, BRTHDTC, SEX) %>%
