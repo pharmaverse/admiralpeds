9af1073e9be8b214e6c78671f100a6dc0ee36351
Ross Farrugia <ross.farrugia@roche.com>
2024-07-09 13:02:28 +0000

    #62 minor updates for devtools check and to make reference page consistent with other functions

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -1,8 +1,8 @@
 #' @keywords internal
 #' @family internal
 #' @importFrom admiraldev assert_data_frame assert_expr assert_symbol assert_varval_list expr_c
-#' @importFrom dplyr arrange bind_rows filter group_by lead left_join mutate pull rename select
+#' @importFrom dplyr arrange bind_cols bind_rows filter group_by lead left_join mutate pull rename select
 #' @importFrom magrittr %>%
 #' @importFrom rlang abort enexpr exprs is_empty
-#' @importFrom stats pnorm qnorm
+#' @importFrom stats approx pnorm qnorm
 "_PACKAGE"
--- R/derive_interp_records.R
+++ R/derive_interp_records.R
@@ -1,23 +1,25 @@
 #' Derive interpolated rows for the CDC charts (>=2 yrs old)
 #'
-#' Derive interpolated rows for the CDC charts (>=2 yrs old) by AGE
-#' for the following paramters: HEIGHT, WEIGHT and BMI
+#' Derive interpolated rows for the CDC charts (>=2 yrs old) by age in days
+#' for the following parameters: HEIGHT, WEIGHT and BMI
 #'
 #' @param dataset Input metadataset
 #'
-#'   The variables "AGE", "AGEU", "SEX", "L", "M", "S" are expected to be in the dataset
-#'   For BMI the additional variables "P95" and "Sigma" are expected to be in the dataset
-#'   Note that "AGE" must be in days so that "AGEU" = "days"
+#'   The variables `AGE`, `AGEU`, `SEX`, `L`, `M`, `S` are expected to be in the dataset
+#'
+#'   For BMI the additional variables `P95` and `Sigma` are expected to be in the dataset
+#'
+#'   Note that `AGE` must be in days so that `AGEU` is equal to `"DAYS"`
 #'
 #' @param parameter CDC/WHO metadata parameter
 #'
-#' *Permitted Values*: "WEIGHT", "HEIGHT" or "BMI" only - Must be not NULL
+#' *Permitted Values*: `"WEIGHT"`, `"HEIGHT"` or `"BMI"` only - Must not be `NULL`
 #'
 #'   e.g. `parameter = "WEIGHT"`,
-#'   or   `parameter = "HEIGHT"`,
+#'        `parameter = "HEIGHT"`,
 #'   or   `parameter = "BMI"`.
 #'
-#' @return The input dataset additional interpolated records with the input dataset records
+#' @return The input dataset plus additional interpolated records
 #'
 #' @family metadata
 #'
@@ -26,23 +28,23 @@
 #' @export
 #'
 #' @examples
-#' library(admiral)
-#' library(admiralpeds)
-#' library(dplyr)
-#' data(cdc_htage)
-#' cdc_htage %>%
-#' mutate(
-#'  SEX = case_when(
-#'     SEX == 1 ~ "M",
-#'     SEX == 2 ~ "F",
-#'     TRUE ~ NA_character_
-#'     ),
-#'   AGE = AGE * 30.4375) %>%
-#' # Interpolate the AGE by SEX
-#' group_by(SEX) %>%
-#' # Ensure first that Age unit is "DAYS"
-#' do(derive_interp_records(., parameter = "HEIGHT")) %>%
-#' ungroup()
+#' library(dplyr, warn.conflicts = FALSE)
+#'
+#' cdc_htage <- admiralpeds::cdc_htage %>%
+#'   mutate(
+#'    SEX = case_when(
+#'       SEX == 1 ~ "M",
+#'       SEX == 2 ~ "F",
+#'       TRUE ~ NA_character_
+#'       ),
+#'     AGE = AGE * 30.4375) %>%
+#'   # Interpolate the AGE by SEX
+#'   group_by(SEX) %>%
+#'   # Ensure first that Age unit is "DAYS"
+#'   do(derive_interp_records(., parameter = "HEIGHT")) %>%
+#'   ungroup()
+#'
+#'   print(cdc_htage)
 derive_interp_records <- function(dataset, parameter = "WEIGHT") {
   stopifnot(parameter %in% c("HEIGHT", "WEIGHT", "BMI"))
   if (parameter %in% c("HEIGHT", "WEIGHT")) {
