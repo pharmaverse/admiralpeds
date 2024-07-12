c0caa3a302091e755851b056a74e33d5c7dcdd47
Fanny-Gautier <fanny.gautier@cytel.com>
2024-07-12 17:11:36 +0200

    #62 update admiralpeds-package.R

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -2,9 +2,10 @@
 #' @family internal
 #' @importFrom admiraldev assert_character_scalar assert_data_frame assert_expr assert_symbol
 #'             assert_vars assert_varval_list expr_c
-#' @importFrom dplyr arrange bind_cols bind_rows filter group_by lead left_join mutate pull rename
-#'             row_number select ungroup
+#' @importFrom dplyr arrange bind_cols bind_rows do filter group_by group_by_at lead
+#'             left_join mutate pull rename row_number select slice ungroup
 #' @importFrom magrittr %>%
 #' @importFrom rlang abort enexpr exprs is_empty
 #' @importFrom stats approx pnorm qnorm
+#' @importFrom tidyselect all_of
 "_PACKAGE"
