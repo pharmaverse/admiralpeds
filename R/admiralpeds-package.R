31633e875a165fc7e4a231309ec8b9e6b96c9710
Fanny-Gautier <fanny.gautier@cytel.com>
2024-07-15 16:57:03 +0200

    #62 delete deprecated do() and group_by_at() from admiralpeds-package.R and NAMESPACE

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -3,7 +3,7 @@
 #' @importFrom admiraldev assert_character_scalar assert_data_frame assert_expr assert_symbol
 #'             assert_vars assert_varval_list expr_c replace_values_by_names
 #' @importFrom cli cli_abort
-#' @importFrom dplyr across arrange bind_cols bind_rows do filter group_by group_by_at lead
+#' @importFrom dplyr across arrange bind_cols bind_rows filter group_by lead
 #'             left_join mutate pull reframe rename row_number select slice ungroup
 #' @importFrom magrittr %>%
 #' @importFrom purrr map
