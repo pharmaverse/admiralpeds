ecffcfed870cd9956fda742ff416169e25a6f10d
Fanny-Gautier <fanny.gautier@cytel.com>
2024-07-15 15:44:41 +0200

    #62 update DESCIPTION, NAMESPACE, admiralpeds-package.R

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -1,10 +1,11 @@
 #' @keywords internal
 #' @family internal
 #' @importFrom admiraldev assert_character_scalar assert_data_frame assert_expr assert_symbol
-#'             assert_vars assert_varval_list expr_c
+#'             assert_vars assert_varval_list expr_c replace_values_by_names
 #' @importFrom dplyr arrange bind_cols bind_rows do filter group_by group_by_at lead
 #'             left_join mutate pull rename row_number select slice ungroup
 #' @importFrom magrittr %>%
+#' @importFrom purrr map
 #' @importFrom rlang abort enexpr exprs is_empty
 #' @importFrom stats approx pnorm qnorm
 #' @importFrom tidyselect all_of
