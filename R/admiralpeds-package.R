991b97f4e0a9f5323ccbf34521aa6e600f94d471
Fanny-Gautier <fanny.gautier@cytel.com>
2024-07-17 14:39:07 +0200

    #73 update admiralpeds-package.R and NAMESPACE

--- R/admiralpeds-package.R
+++ R/admiralpeds-package.R
@@ -1,5 +1,6 @@
 #' @keywords internal
 #' @family internal
+#' @importFrom admiral signal_duplicate_records
 #' @importFrom admiraldev assert_character_scalar assert_data_frame assert_expr assert_symbol
 #'             assert_vars assert_varval_list expr_c replace_values_by_names
 #' @importFrom cli cli_abort
