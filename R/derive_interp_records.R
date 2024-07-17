6a29242c8618938720cc3c70def342d774125778
Fanny-Gautier <fanny.gautier@cytel.com>
2024-07-17 13:04:46 +0200

    #73 return dataset, include AGEU checks, update examples and Value

--- R/derive_interp_records.R
+++ R/derive_interp_records.R
@@ -1,6 +1,6 @@
 #' Derive interpolated rows for the CDC charts (>=2 yrs old)
 #'
-#' Derive interpolated rows for the CDC charts (>=2 yrs old) by age in days
+#' Derive a linear interpolation of rows for the CDC charts (>=2 yrs old) by age in days
 #' for the following parameters: HEIGHT, WEIGHT and BMI
 #'
 #' @param dataset Input metadataset
@@ -24,7 +24,8 @@
 #'        `parameter = "HEIGHT"`,
 #'   or   `parameter = "BMI"`.
 #'
-#' @return The input dataset plus additional interpolated records.
+#' @return The input dataset plus additional interpolated records: a record
+#'         for each day from the minimum age to the maximum age.
 #'
 #' @family metadata
 #'
@@ -44,26 +45,40 @@
 #'       TRUE ~ NA_character_
 #'     ),
 #'     # Ensure first that Age unit is "DAYS"
-#'     AGE = round(AGE * 30.4375)
-#'   ) %>%
-#'   # Interpolate the AGE by SEX
-#'   derive_interp_records(
-#'     by_vars = exprs(SEX),
-#'     parameter = "HEIGHT"
+#'     AGE = round(AGE * 30.4375),
+#'     AGEU = "DAYS"
 #'   )
 #'
-#' print(cdc_htage)
+#' # Interpolate the AGE by SEX
+#' derive_interp_records(
+#'   dataset = cdc_htage,
+#'   by_vars = exprs(SEX),
+#'   parameter = "HEIGHT"
+#' )
 derive_interp_records <- function(dataset,
                                   by_vars = NULL,
                                   parameter) {
   # Apply assertions to each argument to ensure each object is appropriate class
   assert_vars(by_vars, optional = TRUE)
   assert_character_scalar(parameter, values = c("HEIGHT", "WEIGHT", "BMI"))
-  assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, L, M, S))
+  assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, AGEU, L, M, S))
   if (parameter == "BMI") {
-    assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, L, M, S, P95, Sigma))
+    assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, AGEU, L, M, S, P95, Sigma))
   }
 
+  # Ensure to have AGE unit in "Days"
+  ageu <- dataset %>%
+    select(AGEU) %>%
+    filter(toupper(AGEU) != "DAYS") %>%
+    group_by(AGEU) %>%
+    slice(1) %>%
+    ungroup()
+
+  if (nrow(ageu)>0){
+    cli_abort("The Age Unit (AGEU) from the input dataset must be in 'DAYS'")
+  }
+
+  # Sort the data for the interpolation
   arrange(dataset, !!!by_vars, AGE)
 
   # Ensure to have unique combination when by_vars is not defined
@@ -113,4 +128,5 @@ derive_interp_records <- function(dataset,
       ungroup() %>%
       filter(!is.na(AGE))
   }
+  return(dataset)
 }
