bd7098befc20108ffd2c5a138693a2d719bd8c99
Fanny Gautier <157114584+Fanny-Gautier@users.noreply.github.com>
2024-07-24 10:27:28 -0400

    Closes #73 improve derive_interp_records (#76)
    * #73 return dataset, include AGEU checks, update examples and Value
    
    * #73 Include AGEU in metadata_vars
    
    * #73 Ensure uniqueness of all records,
    
    * #73 ADVS template: implement AGEU earlier as required in derive_interp_records()
    
    * #73 update admiralpeds-package.R and NAMESPACE
    
    * #73 keep any potential non-interpolated variables
    
    * #73 Update admiralpeds-package.R and NAMESPACE
    
    * #73 Update unit tests to work with non-interpolated variables kept
    
    * #73 styler
    
    * #73 DESCRIPTION: Imports admiral instead of Suggests
    
    * #73 lintr
    
    * #73 vignette: include AGEU before the interpolation as required in the function
    
    * #73 vignette: styler
    
    * #73 attempt to ignore misfiring lintr checks
    
    * #73 testing working around lintr check
    
    * #73 working around lintr
    
    * #73 improve_derive_interp_records: try to fix lintr workflow
    
    * #73 improve_derive_interp_records: try to fix lintr workflow
    
    * #73 derive_interp_records.R: call correct final dataset
    
    * #73 improve_derive_interp_records: do not install package for lintr workflow
    
    * #73 apply LOCF to the non-interpolated variables
    
    * #73 update admiralpeds-package.R, DESCRIPTION and NAMESPACE
    
    * #73 Update unit tests for different values to apply LOCF
    
    * #73 lintr
    
    * #73 styler
    
    * #73 group_by to apply LOCF to the non-interpolated variables
    
    * #73 improve_derive_interp_records: simplify code and improve documentation
    
    * #73 improve_derive_interp_records: style file
    
    * #73 improve_derive_interp_records: fix spelling
    
    * #73 minor updates
    
    ---------
    
    Co-authored-by: Ross Farrugia <ross.farrugia@roche.com>
    Co-authored-by: Stefan Bundfuss <stefan.bundfuss@roche.com>

--- R/derive_interp_records.R
+++ R/derive_interp_records.R
@@ -1,6 +1,6 @@
 #' Derive interpolated rows for the CDC charts (>=2 yrs old)
 #'
-#' Derive interpolated rows for the CDC charts (>=2 yrs old) by age in days
+#' Derive a linear interpolation of rows for the CDC charts (>=2 yrs old) by age in days
 #' for the following parameters: HEIGHT, WEIGHT and BMI
 #'
 #' @param dataset Input metadataset
@@ -19,12 +19,15 @@
 #' @param parameter CDC/WHO metadata parameter
 #'
 #' *Permitted Values*: `"WEIGHT"`, `"HEIGHT"` or `"BMI"` only - Must not be `NULL`
+#'   e.g. `parameter = "WEIGHT"`, `parameter = "HEIGHT"`, or   `parameter =
+#'   "BMI"`.
 #'
-#'   e.g. `parameter = "WEIGHT"`,
-#'        `parameter = "HEIGHT"`,
-#'   or   `parameter = "BMI"`.
+#' @return The input dataset plus additional interpolated records: a record for
+#'   each day from the minimum age to the maximum age.
 #'
-#' @return The input dataset plus additional interpolated records.
+#'   If any variables in addition to the expected ones are in the input dataset,
+#'   LOCF (Last Observation Carried Forward) is applied to populate them for the
+#'   new records.
 #'
 #' @family metadata
 #'
@@ -44,73 +47,85 @@
 #'       TRUE ~ NA_character_
 #'     ),
 #'     # Ensure first that Age unit is "DAYS"
-#'     AGE = round(AGE * 30.4375)
-#'   ) %>%
-#'   # Interpolate the AGE by SEX
-#'   derive_interp_records(
-#'     by_vars = exprs(SEX),
-#'     parameter = "HEIGHT"
+#'     AGE = round(AGE * 30.4375),
+#'     AGEU = "DAYS"
 #'   )
 #'
-#' print(cdc_htage)
+#' # Interpolate the AGE by SEX
+#' derive_interp_records(
+#'   dataset = cdc_htage,
+#'   by_vars = exprs(SEX),
+#'   parameter = "HEIGHT"
+#' )
 derive_interp_records <- function(dataset,
                                   by_vars = NULL,
                                   parameter) {
   # Apply assertions to each argument to ensure each object is appropriate class
   assert_vars(by_vars, optional = TRUE)
   assert_character_scalar(parameter, values = c("HEIGHT", "WEIGHT", "BMI"))
-  assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, L, M, S))
+  assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, AGEU, L, M, S))
   if (parameter == "BMI") {
-    assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, L, M, S, P95, Sigma))
+    assert_data_frame(dataset, required_vars = exprs(!!!by_vars, AGE, AGEU, L, M, S, P95, Sigma))
   }
 
-  arrange(dataset, !!!by_vars, AGE)
+  # Ensure to have AGE unit in "Days"
+  if (any(toupper(dataset$AGEU) != "DAYS")) {
+    cli_abort("The Age Unit (AGEU) from the input dataset must be in 'DAYS'")
+  }
 
-  # Ensure to have unique combination when by_vars is not defined
-  metadata_vars <- c("AGE", "L", "M", "S")
+  # Sort the data for the interpolation
+  dataset <- arrange(dataset, !!!by_vars, AGE)
+
+  # Define the metadata variables to be interpolated
+  interp_vars <- c("AGE", "L", "M", "S")
   if (parameter == "BMI") {
-    metadata_vars <- append(metadata_vars, c("P95", "Sigma"))
+    interp_vars <- append(interp_vars, c("P95", "Sigma"))
   }
 
-  if (is.null(by_vars)) {
-    other_data_vars <- names(dataset %>% select(-all_of(metadata_vars)))
+  # Ensure the uniqueness of records to interpolate
+  signal_duplicate_records(dataset, by_vars = exprs(!!!by_vars, AGE))
 
-    nb_occ <- nrow(dataset %>%
-      group_by(across(all_of(other_data_vars))) %>%
-      slice(1) %>%
-      ungroup())
-
-    if (nb_occ > 1) {
-      cli_abort(paste0(
-        "The combination of ", paste(other_data_vars, collapse = ", "),
-        " must be unique. Please define `by_vars` otherwise."
-      ))
-    }
-  }
+  # Define the non-interpolated variables and keep the corresponding unique records
+  non_interp_vars <- setdiff(names(dataset), c(interp_vars, by_vars))
+  non_interp_dataset <- dataset %>%
+    select(!!!by_vars, AGE, all_of(non_interp_vars))
 
   # Linear interpolation
   fapp <- function(v, age) {
     approx(age, v, xout = seq(min(age), max(age)))$y
   }
 
+  # Apply LOCF to non-interpolated variables
+  apply_locf <- function(x) {
+    na.locf(x, na.rm = FALSE)
+  }
+
   # Apply the function within each group and combine the results
   if (is.null(by_vars)) {
-    dataset <- dataset %>%
+    interp_dataset <- dataset %>%
       reframe({
         age <- AGE
-        x <- lapply(select(., all_of(metadata_vars)), fapp, age = age)
+        x <- lapply(select(., all_of(interp_vars)), fapp, age = age)
         as.data.frame(do.call(bind_cols, x))
       }) %>%
       filter(!is.na(AGE))
   } else {
-    dataset <- dataset %>%
-      group_by(across(!!!syms(map(replace_values_by_names(by_vars), as_label)))) %>%
+    interp_dataset <- dataset %>%
+      group_by(!!!by_vars) %>%
       reframe({
         age <- AGE
-        x <- lapply(across(all_of(metadata_vars)), fapp, age = age)
+        x <- lapply(across(all_of(interp_vars)), fapp, age = age)
         as.data.frame(do.call(bind_cols, x))
       }) %>%
       ungroup() %>%
       filter(!is.na(AGE))
   }
+
+  # Merge non-interpolated variables (if any) back into the interpolated dataset
+  interp_dataset %>%
+    left_join(non_interp_dataset, by = c(vars2chr(by_vars), "AGE")) %>%
+    group_by(!!!by_vars) %>%
+    # Apply LOCF to the non-interpolated variables
+    mutate(across(all_of(non_interp_vars), apply_locf)) %>%
+    ungroup()
 }
