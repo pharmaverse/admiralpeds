faeb49cd2706e644fc7c157d08cd9501bea8a1a7
Zelos Zhu <zelos.zhu@atorusresearch.com>
2024-07-26 20:11:25 +0000

    weird rounding error showing up, test was originally fine

--- tests/testthat/test-derive_params_growth_height.R
+++ tests/testthat/test-derive_params_growth_height.R
@@ -38,7 +38,7 @@ test_that("derive_params_growth_height Test 1: derive_params_growth_height works
     ((100 / 8)^7 - 1) / (7 * 9),
     ((110 / 11)^10 - 1) / (10 * 12),
     ((100 / 8)^7 - 1) / (7 * 9),
-    ((100 / 8)^7 - 1) / (7 * 9)
+    ((100 / 11)^10 - 1) / (10 * 12)
   )
 
   expect_equal(
