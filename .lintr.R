e33afa12c61c0e3bbaa924edcc7cfacd089fa8c5
Fanny-Gautier <fanny.gautier@cytel.com>
2025-11-20 16:44:41 +0100

    #113 Update .lintr.R comments - test CI/CD checks

--- .lintr.R
+++ .lintr.R
@@ -1,11 +1,11 @@
-# source in temporary environment to avoid changing the global environment
+# Source in temporary environment to avoid changing the global environment
 temp_env <- new.env(parent = globalenv())
 
 source(system.file("lintr/linters.R", package = "admiraldev"), local = temp_env)
 
 linters <- temp_env$admiral_linters()
 
-# remove temporary environment to avoid lintr warning regarding "unused settings"
+# Remove temporary environment to avoid lintr warning regarding "unused settings"
 rm(temp_env)
 
 exclusions <- list(
