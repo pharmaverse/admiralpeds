94fdda9f0685f2c11fec91b526e5d29bd29f7577
Fanny-Gautier <fanny.gautier@cytel.com>
2025-11-21 12:14:21 +0100

    #112 Add permitted values to rdx_meta.R

--- man/roxygen/rdx_meta.R
+++ man/roxygen/rdx_meta.R
@@ -2,7 +2,7 @@ list(
   rdx_permitted_values = list(
     boolean = "`TRUE`, `FALSE`",
     char_scalar = "a character scalar, i.e., a character vector of length one",
-    condition = "an unquoted condition, e.g., `AVISIT == \"BASELINE\"`",
+    condition = "an unquoted condition, e.g., `parameter = VSTESTCD == \"WEIGHT\"`",
     dataset = "a dataset, i.e., a `data.frame` or tibble",
     dataset_list = "named list of datasets, e.g., `list(adsl = adsl, ae = ae)`",
     date = "a date or datetime variable",
@@ -22,10 +22,12 @@ list(
     expr_list_summary = "list of named expressions created by `exprs()`, e.g., `exprs(CUMDOSA = sum(AVAL, na.rm = TRUE), AVALU = \"ml\")`",
     flag_event = "a list of `flag_event()` objects",
     join_type = "`\"before\"`, `\"after\"`, `\"all\"`",
+    logic_scalar = "a logical scalar, i.e., a logical vector of length one, e.g. `TRUE`/`FALSE`",
     merge_rel = "`\"one-to-one\"`, `\"many-to-one\"`",
     mode = "`\"first\"`, `\"last\"`",
     msg = "a console message to be printed, e.g. `\"Attention\"` or for longer messages use `paste(\"Line 1\", \"Line 2\")`",
     msg_type = "`\"none\"`, `\"message\"`, `\"warning\"`, `\"error\"`",
+    num_scalar = "a numeric scalar, i.e., a numeric vector of length one",
     order_optional = "list of expressions created by `exprs()`, e.g., `exprs(ADT, desc(AVAL))` or `NULL`",
     pos_int = "a positive integer, e.g. `2` or `5`",
     source_list = "a list of source objects, e.g., `list(pd, death)`",
@@ -34,6 +36,7 @@ list(
     var_list = "list of variables created by `exprs()`, e.g., `exprs(USUBJID, VISIT)`",
     var_list_rename = "list of (optionally named) variables created by `exprs()`, e.g., `exprs(USUBJID, ADY = ASTDY)`",
     var_list_tidyselect = "list of variables or tidyselect expressions created by `exprs()`, e.g., `exprs(DTHDT, starts_with(\"AST\"))` or `exprs(everything)`",
+    var_list_value_pairs = "List of variable-value pairs",
     var_expr_list = "list of variables or named expressions created by `exprs()`, e.g., `exprs(EXSTDY, EXSTDTM = convert_dtc_to_dtm(EXSTDTC))`"
   )
 )
